d0172a79007609ecd59be28d10424499ac17907a
Navee <raozhanghui@gmail.com>
2019-04-09 23:41:44 +0800

    初始化项目

---
+++ src/main/java/cc/navee/bootiful/admin/BootifulAdminApplication.java
@@ -0,0 +1,13 @@
+package cc.navee.bootiful.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifulAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootifulAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cc/navee/bootiful/admin/BootifulAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package cc.navee.bootiful.admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootifulAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
