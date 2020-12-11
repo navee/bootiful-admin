0b39f38c4349ecac1d8c203cfd8da0dc9bf667ea
raozhanghui <raozhanghui@yijiupi.com>
2020-12-11 16:30:16 +0800

    init

---
+++ src/main/java/io/github/navee/bootifuladmin/BootifulAdminApplication.java
@@ -0,0 +1,13 @@
+package io.github.navee.bootifuladmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifulAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootifulAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/navee/bootifuladmin/BootifulAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.navee.bootifuladmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootifulAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
